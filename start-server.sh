5f0127254a2608f7c853bd3806dd2b2408f48c50
Z User <z@container>
2026-06-11 23:47:45 +0000

    09979bb8-bd52-4f1a-940b-1acd75418fde

--- start-server.sh
+++ start-server.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 cd /home/z/my-project
-while true; do
-  echo "Starting Next.js server..."
-  NODE_OPTIONS='--max-old-space-size=256' npx next start -H 0.0.0.0 -p 3000 2>&1
-  EXIT_CODE=$?
-  echo "Server exited with code $EXIT_CODE, restarting in 3 seconds..."
-  sleep 3
-done
+NODE_OPTIONS='--max-old-space-size=1024' exec /home/z/my-project/node_modules/.bin/next start -H 0.0.0.0 -p 3000
